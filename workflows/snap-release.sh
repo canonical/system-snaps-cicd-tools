da9333e2a49c53fb91e8a63030d14f5d9b885c36
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-02-07 19:58:53 +0000

    workflows/snap-release.sh: make sure NEXT_VERSION is empty
    if not set externally.

--- workflows/snap-release.sh
+++ workflows/snap-release.sh
@@ -371,5 +371,5 @@ if [ $# -ne 2 ]; then
     printf "Usage: %s <release_branch> <workspace_dir>\n" "$0"
     exit 1
 fi
-NEXT_VERSION=${NEXT_VERSION:-default}
+NEXT_VERSION=${NEXT_VERSION:-}
 main "$1" "$2" "$GIT_REPO" "$BUILD_BRANCH" "$NEXT_VERSION"
