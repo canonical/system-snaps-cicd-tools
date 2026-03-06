eac0c4ecd23a500c9b0e706154af106dae963ea9
Philip Meulengracht <the_meulengracht@hotmail.com>
2026-03-06 13:44:32 +0100

    snapd-lib: add missing image links

--- snapd-lib/image.sh
+++ snapd-lib/image.sh
@@ -34,6 +34,12 @@ get_google_image_url_for_vm() {
         ubuntu-24.04-arm-64*)
             echo "https://storage.googleapis.com/snapd-spread-tests/images/cloudimg/noble-server-cloudimg-arm64.img"
             ;;
+        ubuntu-26.04-64*)
+            echo "https://storage.googleapis.com/snapd-spread-tests/images/cloudimg/resolute-server-cloudimg-amd64.img"
+            ;;
+        ubuntu-26.04-arm-64*)
+            echo "https://storage.googleapis.com/snapd-spread-tests/images/cloudimg/resolute-server-cloudimg-arm64.img"
+            ;;
         *)
             echo "unsupported system"
             exit 1
@@ -70,6 +76,12 @@ get_ubuntu_image_url_for_vm() {
             ;;
         ubuntu-24.04-arm-64*)
             echo "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-arm64.img"
+            ;;
+        ubuntu-26.04-64*)
+            echo "https://cloud-images.ubuntu.com/resolute/current/resolute-server-cloudimg-amd64.img"
+            ;;
+        ubuntu-26.04-arm-64*)
+            echo "https://cloud-images.ubuntu.com/resolute/current/resolute-server-cloudimg-arm64.img"
             ;;
         *)
             echo "unsupported system"
